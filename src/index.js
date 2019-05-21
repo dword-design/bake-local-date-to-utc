4de95f0eaeffb7121c067785e20672fb27bcf65e
Sebastian Landwehr <info@dword-design.de>
2019-05-21 19:37:57 +0200

    init

---
+++ src/index.js
@@ -0,0 +1,10 @@
+export default date => new Date(
+  Date.UTC(
+    date.getFullYear(),
+    date.getMonth(),
+    date.getDate(),
+    date.getHours(),
+    date.getMinutes(),
+    date.getSeconds()
+  )
+)
