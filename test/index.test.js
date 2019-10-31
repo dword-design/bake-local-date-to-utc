54f2b8a67a6bf93a53f576d337b511c227788562
Sebastian Landwehr <info@dword-design.de>
2019-10-31 01:22:24 +0100

    upgrade base

--- test/index.test.js
+++ test/index.test.js
@@ -1,3 +1,6 @@
 import bakeLocalDateToUtc from '@dword-design/bake-local-date-to-utc'
 
-it('works', () => expect(bakeLocalDateToUtc(new Date('2019-10-27T19:10:32.000Z')).toISOString()).toEqual('2019-10-27T19:10:32.000Z'))
+describe('index', () => {
+
+  it('works', () => expect(bakeLocalDateToUtc(new Date('2019-10-27T19:10:32.000Z')).toISOString()).toEqual('2019-10-27T20:10:32.000Z'))
+})
